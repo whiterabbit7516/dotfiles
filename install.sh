799dc1a550727dbe266f6b2b179a50cd8daacb65
xanthos954 <xanthos954>
2020-02-23 01:21:29 +0000

    install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+# powershell
+cp ./powershell/profile.ps1 ~/.config/powershell/profile.ps1
+
+# vim todo
+
+# tmux todo
+
+# vscode
+cp ./vscode/settings.json ~/.config/Code/User/settings.json
+cp ./vscode/keybindings.json ~/.config/Code/User/keybindings.json
