404bee939b37a23948373331e2817ae198eb2046
xanthos954 <xanthos954>
2020-02-23 01:17:11 +0000

    install.ps1

--- install.ps1
+++ install.ps1
@@ -1,1 +1,10 @@
+# powershell
+Copy-Item $PSScriptRoot/powershell/profile.ps1 $PROFILE
 
+# vim todo
+
+# tmux todo
+
+# vscode
+Copy-Item $PSScriptRoot/vscode/settings.json ~/.config/Code/User/settings.json
+Copy-Item $PSScriptRoot/vscode/keybindings.json ~/.config/Code/User/keybindings.json
---
+++ powershell/profile.ps1
@@ -0,0 +1,27 @@
+#########################################
+# aliases
+#########################################
+# Set-Alias -Name vscode -Value '/snap/bin/code'
+#########################################
+# paths
+#########################################
+
+#########################################
+
+# Set prompt
+Import-Module posh-git -ErrorAction SilentlyContinue
+
+if ($?)
+{
+    $GitPromptSettings.DefaultPromptPrefix = "`$ENV:USER@`$(hostname) | "
+    $GitPromptSettings.DefaultPromptPath = "`$(Get-Location | Split-Path -Leaf)"
+    $GitPromptSettings.DefaultPromptSuffix = " > "
+}
+else 
+{
+    function prompt {
+        $path = Get-Location | Split-Path -Leaf
+        $prompt = "[$ENV:USER@$(hostname)] $path > "
+        return $prompt
+    }
+}
