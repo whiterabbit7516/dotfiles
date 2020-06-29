264fb386100419773574bd279a9262c2e93724c3
Hassan Ali <hassan.ali.3821@outlook.com>
2020-06-29 14:59:43 +0100

    Update profile.ps1

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -16,48 +16,65 @@ elseif ($(command code-insiders -ErrorAction SilentlyContinue) -ne $null)
 #########################################
 # paths
 #########################################
+#########################################
 # argument completers
 #########################################
-# https://www.dennisroche.com/aws-cli-on-windows/ 
-Register-ArgumentCompleter -Native -CommandName aws -ScriptBlock {
-  param($commandName, $wordToComplete, $cursorPosition)
-  $ENV:COMP_LINE=$wordToComplete
-  $ENV:COMP_POINT=$cursorPosition
-  aws_completer "$wordToComplete" $cursorPosition | ForEach-Object {
-      [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-  }
-}
+# # https://www.dennisroche.com/aws-cli-on-windows/ 
+# Register-ArgumentCompleter -Native -CommandName aws -ScriptBlock {
+#   param($commandName, $wordToComplete, $cursorPosition)
+#   $ENV:COMP_LINE=$wordToComplete
+#   $ENV:COMP_POINT=$cursorPosition
+#   aws_completer "$wordToComplete" $cursorPosition | ForEach-Object {
+#       [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+#   }
+# }
 #########################################
 # PSReadLine
+#########################################
 Set-PSReadLineOption -EditMode Vi -ViModeIndicator Cursor
 Set-PSReadLineKeyHandler -Chord Ctrl+c -Function ViCommandMode
 #########################################
 # FZF
+#########################################
 Import-Module PSFzf -ArgumentList 'Ctrl+t','Ctrl+r' -ErrorAction SilentlyContinue
 #########################################
-# Set prompt
-Import-Module posh-git -ErrorAction SilentlyContinue
-
-if ($?)
-{
-    $GitPromptSettings.DefaultForegroundColor = [ConsoleColor]::Magenta
-    $GitPromptSettings.DefaultPromptPrefix = "`$ENV:USER@`$(hostname) "
-    # $GitPromptSettings.DefaultPromptPath = "`$(Get-Location | Split-Path -Leaf)"
-    $GitPromptSettings.DefaultPromptSuffix = " `n■ "
-}
-else 
-{
-    function prompt {
-        $path = Get-Location | Split-Path -Leaf
-        $prompt = "[$ENV:USER@$(hostname)] $path > "
-        return $prompt
-    }
-}
+# PoshGit
 #########################################
-# Print nordvpn status
+# Import-Module posh-git -ErrorAction SilentlyContinue
+# if ($?)
+# {
+#     $GitPromptSettings.DefaultForegroundColor = [ConsoleColor]::Magenta
+#     $GitPromptSettings.DefaultPromptPrefix = "`$ENV:USER@`$(hostname) "
+#     # $GitPromptSettings.DefaultPromptPath = "`$(Get-Location | Split-Path -Leaf)"
+#     $GitPromptSettings.DefaultPromptSuffix = " `n■ "
+# }
+#########################################
+# VPN
+#########################################
 $nordvpn = command nordvpn -ErrorAction SilentlyContinue
 if ($nordvpn -ne $null)
 {
   nordvpn status | select-string -Pattern "Status|IP|server" -NoEmphasis
+}
+else
+{
+  Write-Warning "VPN Unavailable"
+}
+#########################################
+# Prompt
+#########################################
+$HOST.UI.RawUI.ForegroundColor = 6
+function prompt {
+  $path = Get-Location # | Split-Path -Leaf
+  $prompt = "[$ENV:USER@$(hostname)] $path`n■ ■ ■ "
+  return $prompt
+}
+#########################################
+# .local/profile.ps1
+#########################################
+if (Test-Path ~/.local/powershell/profile.ps1)
+{
+  Write-Information "Reading ~/.local/powershell/profile.ps1"
+  Invoke-Expression -Command ~/.local/powershell/profile.ps1
 }
 #########################################
