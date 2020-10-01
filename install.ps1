12f74fd7b145e69887fcfd7bb59b996a5b26aae5
Hassan Ali <hassan.ali.3821@outlook.com>
2020-10-01 22:14:32 +0000

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,7 @@
 ##########################################
 # powershell
 ##########################################
+new-item -type dir ~/.config/powershell -force | out-null
 ln -fsv $PSScriptRoot/powershell/profile.ps1 ~/.config/powershell/profile.ps1
 
 ##########################################
@@ -17,6 +18,7 @@ ln -fsv $PSScriptRoot/tmux/.tmux.conf ~/.tmux.conf
 ##########################################
 # vscode
 ##########################################
+new-item -type dir ~/.config/Code/User -force | out-null
 # ln -fsv $PSScriptRoot/vscode/settings.json ~/.config/Code/User/settings.json
 # ln -fsv $PSScriptRoot/vscode/keybindings.json ~/.config/Code/User/keybindings.json
 copy-item $PSScriptRoot/vscode/settings.json ~/.config/Code/User/settings.json
