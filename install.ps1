ddb967a2ac9ae86eb7b2254fbd942b9fb23a4412
whiterabbit7516 <19978855+whiterabbit7516@users.noreply.github.com>
2025-01-20 01:44:47 +0000

    cleanup

--- install.ps1
+++ install.ps1
@@ -4,23 +4,18 @@
 install-module psfzf -force
 new-item -type dir ~/.config/powershell -force | out-null
 ln -fsv $PSScriptRoot/powershell/profile.ps1 ~/.config/powershell/profile.ps1
-new-item -type dir ~/.local/dotfiles/powershell -force | out-null
-cp -rnv $PSScriptRoot/powershell/local/* ~/.local/dotfiles/powershell
-
+new-item -type file ~/.local/dotfiles/powershell/profile.ps1 -force | out-null
 ##########################################
 # vim
 ##########################################
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -fsv $PSScriptRoot/vim/.vimrc ~/.vimrc
-
 ##########################################
 # tmux
 ##########################################
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -fsv $PSScriptRoot/tmux/.tmux.conf ~/.tmux.conf
-new-item -type dir ~/.local/dotfiles/tmux -force | out-null
-cp -rnv $PSScriptRoot/tmux/local/* ~/.local/dotfiles/tmux
-
+new-item -type file ~/.local/dotfiles/tmux/tmux.conf -force | out-null
 ##########################################
 # vscode
 ##########################################
@@ -29,7 +24,6 @@ new-item -type dir ~/.config/Code/User -force | out-null
 # ln -fsv $PSScriptRoot/vscode/keybindings.json ~/.config/Code/User/keybindings.json
 copy-item $PSScriptRoot/vscode/settings.json ~/.config/Code/User/settings.json
 copy-item $PSScriptRoot/vscode/keybindings.json ~/.config/Code/User/keybindings.json
-
 ##########################################
 # bash
 ##########################################
--- install.sh
+++ install.sh
@@ -4,22 +4,19 @@
 mkdir -p ~/.config/powershell
 ln -fsv $PWD/powershell/profile.ps1 ~/.config/powershell/profile.ps1
 mkdir -p ~/.local/dotfiles/powershell
-cp -rnv $PWD/powershell/local/* ~/.local/dotfiles/powershell
-
+touch ~/.local/dotfiles/powershell/profile.ps1
 ##########################################
 # vim
 ##########################################
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -fsv $PWD/vim/.vimrc ~/.vimrc
-
 ##########################################
 # tmux
 ##########################################
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -fsv $PWD/tmux/.tmux.conf ~/.tmux.conf
 mkdir -p ~/.local/dotfiles/tmux
-cp -rnv $PWD/tmux/local/* ~/.local/dotfiles/tmux
-
+touch ~/.local/dotfiles/tmux/tmux.conf
 ##########################################
 # vscode
 ##########################################
@@ -28,7 +25,6 @@ mkdir -p ~/.config/Code/User
 # ln -fsv $PWD/vscode/keybindings.json ~/.config/Code/User/keybindings.json
 cp $PWD/vscode/settings.json ~/.config/Code/User/settings.json
 cp $PWD/vscode/keybindings.json ~/.config/Code/User/keybindings.json
-
 ##########################################
 # bash
 ##########################################
